1f307f7a86d2f04458bd80caeffa2ec6a7b7c013
elitzca <elitzca0raycheva@gmail.com>
2018-10-11 21:35:12 +0200

    all working

--- script.js
+++ script.js
@@ -31,11 +31,12 @@ function update() {
   createTapGraphContainers(data.taps);
 
   showBeersSold(data);
+  showBeerImage(data);
 }
 
 function handleQueue(queue) {
   document.querySelector(" #queue h2").textContent = queue.length;
-  document.querySelector("#queue #qv").style.width = 56 * queue.length + "px";
+  document.querySelector("#queue #qv").style.width = 30 * queue.length + "px";
 }
 
 function handleBartenders(taps) {
@@ -50,6 +51,12 @@ function bartenders(bartenders) {
   all.forEach((div, index) => {
     div.querySelector("h1").textContent = bartenders[index].name;
     div.querySelector(".state").textContent = bartenders[index].status;
+    if (bartenders[index].servingCustomer >= 1) {
+      div.querySelector(".serving").textContent =
+        bartenders[index].servingCustomer;
+    } else {
+      div.querySelector(".serving").textContent = "none";
+    }
   });
 }
 
@@ -60,10 +67,19 @@ function stateBartenders(bartenders) {
 function taskBartenders(bartenders) {
   const all = document.querySelectorAll(".bartender");
   all.forEach((div, index) => {
+    //console.log(bartenders[index].statusDetail);
     if (bartenders[index].statusDetail === "pourBeer") {
-      div.querySelector("img").src = "svg/beer.svg";
+      div.querySelector("img").src = "svg/pour-beer_icon.png";
     } else if (bartenders[index].statusDetail === "startServing") {
-      div.querySelector("img").src = "svg/serving.svg";
+      div.querySelector("img").src = "svg/bartender-white.png";
+    } else if (bartenders[index].statusDetail === "releaseTap") {
+      div.querySelector("img").src = "svg/relese-tab_icon.png";
+    } else if (bartenders[index].statusDetail === "waiting") {
+      div.querySelector("img").src = "svg/waiting.png";
+    } else if (bartenders[index].statusDetail === "receivePayment") {
+      div.querySelector("img").src = "svg/recive_money.png";
+    } else if (bartenders[index].statusDetail === "reserveTap") {
+      div.querySelector("img").src = "svg/reserve-tab_icon.svg";
     }
   });
 }
@@ -77,10 +93,34 @@ function createTapGraphContainers(taps) {
     let newHeight = (tap.level * 200) / tap.capacity + "px";
     let newY = 200 - (tap.level * 200) / tap.capacity + "px";
 
-    clone.querySelector("h1").textContent = tap.id;
+    clone.querySelector("h1").textContent = tap.beer;
     clone.querySelector(".beerCont").style.height = "200px";
     clone.querySelector(".beerLiquid").style.height = newHeight;
     clone.querySelector(".beerLiquid").style.top = newY;
+
+    /*  if (tap.beer == "El Hefe") {
+      rightImage = "images/elhefe.png";
+    } else if (tap.beer == "Row 26") {
+      rightImage = "images/row26.png";
+    } else if (tap.beer == "Sleighride") {
+      rightImage = "images/sleighride.png";
+    } else if (tap.beer == "Hollaback Lager") {
+      rightImage = "images/hollaback.png";
+    } else if (tap.beer == "Ruined Childhood") {
+      rightImage = "images/ruinedchildhood.png";
+    } else if (tap.beer == "Hoppily Ever After") {
+      rightImage = "images/hoppilyeverafter.png";
+    } else if (tap.beer == "Fairy Tale Ale") {
+      rightImage = "images/fairytaleale.png";
+    } else if (tap.beer == "Mowintime") {
+      rightImage = "images/mowintime.png";
+    } else if (tap.beer == "GitHop") {
+      rightImage = "images/githop.png";
+    } else {
+      rightImage = "images/steampunk.png";
+    }
+
+    clone.querySelector(".beerImage").src = rightImage; */
 
     clone.querySelector(
       ".thingyTop"
@@ -116,13 +156,13 @@ function showBeersSold(data) {
   let happilyEverAfter = document.querySelector("#happilyEverAfterDiv");
   let ruinedChildhood = document.querySelector("#ruinedChildhoodDiv");
 
-  elHeve.style.width = data.beertypes[0].popularity * 51 + "px";
-  gitHop.style.width = data.beertypes[1].popularity * 51 + "px";
-  mowinTime.style.width = data.beertypes[2].popularity * 51 + "px";
-  fairytaleAle.style.width = data.beertypes[3].popularity * 51 + "px";
-  row26.style.width = data.beertypes[4].popularity * 51 + "px";
-  happilyEverAfter.style.width = data.beertypes[5].popularity * 51 + "px";
-  ruinedChildhood.style.width = data.beertypes[6].popularity * 51 + "px";
+  elHeve.style.width = ((2500 - data.taps[0].level) / 50) * 20 + "px";
+  gitHop.style.width = ((2500 - data.taps[1].level) / 50) * 20 + "px";
+  mowinTime.style.width = ((2500 - data.taps[2].level) / 50) * 20 + "px";
+  fairytaleAle.style.width = ((2500 - data.taps[3].level) / 50) * 20 + "px";
+  row26.style.width = ((2500 - data.taps[4].level) / 50) * 20 + "px";
+  happilyEverAfter.style.width = ((2500 - data.taps[5].level) / 50) * 20 + "px";
+  ruinedChildhood.style.width = ((2500 - data.taps[6].level) / 50) * 20 + "px";
 
   let p1 = document.querySelector("#p1");
   let p2 = document.querySelector("#p2");
@@ -132,11 +172,175 @@ function showBeersSold(data) {
   let p6 = document.querySelector("#p6");
   let p7 = document.querySelector("#p7");
 
-  p1.textContent = data.beertypes[0].popularity + a;
-  p2.textContent = data.beertypes[1].popularity + b;
-  p3.textContent = data.beertypes[2].popularity + c;
-  p4.textContent = data.beertypes[3].popularity + d;
-  p5.textContent = data.beertypes[4].popularity + e;
-  p6.textContent = data.beertypes[5].popularity + f;
-  p7.textContent = data.beertypes[6].popularity + g;
+  p1.textContent = (2500 - data.taps[0].level) / 50;
+  p2.textContent = (2500 - data.taps[1].level) / 50;
+  p3.textContent = (2500 - data.taps[2].level) / 50;
+  p4.textContent = (2500 - data.taps[3].level) / 50;
+  p5.textContent = (2500 - data.taps[4].level) / 50;
+  p6.textContent = (2500 - data.taps[5].level) / 50;
+  p7.textContent = (2500 - data.taps[6].level) / 50;
+}
+
+function showBeerImage(data) {
+  let first = document.querySelector("#first");
+  let second = document.querySelector("#second");
+  let third = document.querySelector("#third");
+  let fourth = document.querySelector("#fourth");
+  let fifth = document.querySelector("#fifth");
+  let sixth = document.querySelector("#sixth");
+  let seventh = document.querySelector("#seventh");
+
+  if (data.taps[0].beer == "El Hefe") {
+    first.src = "images/elhefe.png";
+  } else if (data.taps[0].beer == "Row 26") {
+    first.src = "images/row26.png";
+  } else if (data.taps[0].beer == "Sleighride") {
+    first.src = "images/sleighride.png";
+  } else if (data.taps[0].beer == "Hollaback Lager") {
+    first.src = "images/hollaback.png";
+  } else if (data.taps[0].beer == "Ruined Childhood") {
+    first.src = "images/ruinedchildhood.png";
+  } else if (data.taps[0].beer == "Hoppily Ever After") {
+    first.src = "images/hoppilyeverafter.png";
+  } else if (data.taps[0].beer == "Fairy Tale Ale") {
+    first.src = "images/fairytaleale.png";
+  } else if (data.taps[0].beer == "Mowintime") {
+    first.src = "images/mowintime.png";
+  } else if (data.taps[0].beer == "GitHop") {
+    first.src = "images/githop.png";
+  } else {
+    first.src = "images/steampunk.png";
+  }
+
+  if (data.taps[1].beer == "El Hefe") {
+    second.src = "images/elhefe.png";
+  } else if (data.taps[1].beer == "Row 26") {
+    second.src = "images/row26.png";
+  } else if (data.taps[1].beer == "Sleighride") {
+    second.src = "images/sleighride.png";
+  } else if (data.taps[1].beer == "Hollaback Lager") {
+    second.src = "images/hollaback.png";
+  } else if (data.taps[1].beer == "Ruined Childhood") {
+    second.src = "images/ruinedchildhood.png";
+  } else if (data.taps[1].beer == "Hoppily Ever After") {
+    second.src = "images/hoppilyeverafter.png";
+  } else if (data.taps[1].beer == "Fairy Tale Ale") {
+    second.src = "images/fairytaleale.png";
+  } else if (data.taps[1].beer == "Mowintime") {
+    second.src = "images/mowintime.png";
+  } else if (data.taps[1].beer == "GitHop") {
+    second.src = "images/githop.png";
+  } else {
+    second.src = "images/steampunk.png";
+  }
+
+  if (data.taps[2].beer == "El Hefe") {
+    third.src = "images/elhefe.png";
+  } else if (data.taps[2].beer == "Row 26") {
+    third.src = "images/row26.png";
+  } else if (data.taps[2].beer == "Sleighride") {
+    third.src = "images/sleighride.png";
+  } else if (data.taps[2].beer == "Hollaback Lager") {
+    third.src = "images/hollaback.png";
+  } else if (data.taps[2].beer == "Ruined Childhood") {
+    third.src = "images/ruinedchildhood.png";
+  } else if (data.taps[2].beer == "Hoppily Ever After") {
+    third.src = "images/hoppilyeverafter.png";
+  } else if (data.taps[2].beer == "Fairy Tale Ale") {
+    third.src = "images/fairytaleale.png";
+  } else if (data.taps[2].beer == "Mowintime") {
+    third.src = "images/mowintime.png";
+  } else if (data.taps[2].beer == "GitHop") {
+    third.src = "images/githop.png";
+  } else {
+    third.src = "images/steampunk.png";
+  }
+
+  if (data.taps[3].beer == "El Hefe") {
+    fourth.src = "images/elhefe.png";
+  } else if (data.taps[3].beer == "Row 26") {
+    fourth.src = "images/row26.png";
+  } else if (data.taps[3].beer == "Sleighride") {
+    fourth.src = "images/sleighride.png";
+  } else if (data.taps[3].beer == "Hollaback Lager") {
+    fourth.src = "images/hollaback.png";
+  } else if (data.taps[3].beer == "Ruined Childhood") {
+    fourth.src = "images/ruinedchildhood.png";
+  } else if (data.taps[3].beer == "Hoppily Ever After") {
+    fourth.src = "images/hoppilyeverafter.png";
+  } else if (data.taps[3].beer == "Fairy Tale Ale") {
+    fourth.src = "images/fairytaleale.png";
+  } else if (data.taps[3].beer == "Mowintime") {
+    fourth.src = "images/mowintime.png";
+  } else if (data.taps[3].beer == "GitHop") {
+    fourth.src = "images/githop.png";
+  } else {
+    fourth.src = "images/steampunk.png";
+  }
+
+  if (data.taps[4].beer == "El Hefe") {
+    fifth.src = "images/elhefe.png";
+  } else if (data.taps[4].beer == "Row 26") {
+    fifth.src = "images/row26.png";
+  } else if (data.taps[4].beer == "Sleighride") {
+    fifth.src = "images/sleighride.png";
+  } else if (data.taps[4].beer == "Hollaback Lager") {
+    fifth.src = "images/hollaback.png";
+  } else if (data.taps[4].beer == "Ruined Childhood") {
+    fifth.src = "images/ruinedchildhood.png";
+  } else if (data.taps[4].beer == "Hoppily Ever After") {
+    fifth.src = "images/hoppilyeverafter.png";
+  } else if (data.taps[4].beer == "Fairy Tale Ale") {
+    fifth.src = "images/fairytaleale.png";
+  } else if (data.taps[4].beer == "Mowintime") {
+    fifth.src = "images/mowintime.png";
+  } else if (data.taps[4].beer == "GitHop") {
+    fifth.src = "images/githop.png";
+  } else {
+    fifth.src = "images/steampunk.png";
+  }
+
+  if (data.taps[5].beer == "El Hefe") {
+    sixth.src = "images/elhefe.png";
+  } else if (data.taps[5].beer == "Row 26") {
+    sixth.src = "images/row26.png";
+  } else if (data.taps[5].beer == "Sleighride") {
+    sixth.src = "images/sleighride.png";
+  } else if (data.taps[5].beer == "Hollaback Lager") {
+    sixth.src = "images/hollaback.png";
+  } else if (data.taps[5].beer == "Ruined Childhood") {
+    sixth.src = "images/ruinedchildhood.png";
+  } else if (data.taps[5].beer == "Hoppily Ever After") {
+    sixth.src = "images/hoppilyeverafter.png";
+  } else if (data.taps[5].beer == "Fairy Tale Ale") {
+    sixth.src = "images/fairytaleale.png";
+  } else if (data.taps[5].beer == "Mowintime") {
+    sixth.src = "images/mowintime.png";
+  } else if (data.taps[5].beer == "GitHop") {
+    sixth.src = "images/githop.png";
+  } else {
+    sixth.src = "images/steampunk.png";
+  }
+
+  if (data.taps[6].beer == "El Hefe") {
+    seventh.src = "images/elhefe.png";
+  } else if (data.taps[6].beer == "Row 26") {
+    seventh.src = "images/row26.png";
+  } else if (data.taps[6].beer == "Sleighride") {
+    seventh.src = "images/sleighride.png";
+  } else if (data.taps[6].beer == "Hollaback Lager") {
+    seventh.src = "images/hollaback.png";
+  } else if (data.taps[6].beer == "Ruined Childhood") {
+    seventh.src = "images/ruinedchildhood.png";
+  } else if (data.taps[6].beer == "Hoppily Ever After") {
+    seventh.src = "images/hoppilyeverafter.png";
+  } else if (data.taps[6].beer == "Fairy Tale Ale") {
+    seventh.src = "images/fairytaleale.png";
+  } else if (data.taps[6].beer == "Mowintime") {
+    seventh.src = "images/mowintime.png";
+  } else if (data.taps[6].beer == "GitHop") {
+    seventh.src = "images/githop.png";
+  } else {
+    seventh.src = "images/steampunk.png";
+  }
 }
